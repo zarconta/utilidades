4cf4a039b97f7fc8da78ff5941955953a3cacc49
Javier Murcia <javiermurcia@hotmail.com>
2019-10-18 18:50:22 -0500

    primier comit

---
+++ src/Utilidades.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace zarconta\utilidades;
+/**
+ *  calse principal de 
+ */
+class Utilidades{
+	
+	function __construct()
+	{
+		print 'Hola desde utilidades.! Ing. Javier Murcia.';
+	}
+}
+
+ ?>
---
+++ test/indice.php
@@ -0,0 +1,11 @@
+<?php
+
+
+require __DIR__.'/../vendor/autoload.php';
+
+$saludo = new zarconta\utilidades\Utilidades();
+
+print 'ok';
+
+?>
+
