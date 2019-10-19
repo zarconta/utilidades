53c4668285dee79858b34ae1081b5e55744c1c9b
Javier Murcia <javiermurcia@hotmail.com>
2019-10-19 08:40:00 -0500

    Version 0.2.2

--- src/Utilidades.php
+++ src/Utilidades.php
@@ -8,7 +8,7 @@ class Utilidades{
 	
 	function __construct()
 	{
-		print 'Hola desde utilidades.! original. debe actualizar. bien lo hace . con version 0.2.1 ';
+		print 'Hola desde utilidades.! original. debe actualizar. bien lo hace . con version 0.2.2 actulizada de nuevo  ';
 	}
 }
 
